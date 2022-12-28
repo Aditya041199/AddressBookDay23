bf14afc78bd44b708d7cdd132a152ea85e043e6d
aditya patil <Aditya@example.com>
2022-12-28 20:40:10 +0530

    [Aditya]Add:Added Program To Create Basic Contact Book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using AddressBookDay23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program");
+            AddressBook ab = new AddressBook("Aditya", "Patil", "New Posari", "Mumbai", "Maharashtra", "patiladi411@gmail.com", 410222, 8308508570);
+            ab.DisplayContacts();
+        }
+    }
+}
